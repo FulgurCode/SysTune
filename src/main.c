451df2d7f5e358ce8b21276d170412d1aec1f992
Shreyas sk <shreyassk.dev@gmail.com>
2025-02-22 13:53:18 +0500

    feat: Added style support

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include "window/window.h"
 #include <gtk/gtk.h>
+#include <stdio.h>
+
+static void load_css(void) {
+    GtkCssProvider *provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+
+    const gchar *css_file_path = "styles/main.css";
+    gtk_css_provider_load_from_path(provider, css_file_path);
+
+    gtk_style_context_add_provider_for_display(
+        display,
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    g_object_unref(provider);
+}
+
 
 static void activate(GtkApplication *app, gpointer user_data) {
+  load_css();
+
   GtkWidget *window = create_main_window(app);
   gtk_widget_set_visible(window, TRUE);
 }
@@ -9,6 +29,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
 int main(int argc, char *argv[]) {
   GtkApplication *app =
       gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+
+
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 
   int status = g_application_run(G_APPLICATION(app), argc, argv);
